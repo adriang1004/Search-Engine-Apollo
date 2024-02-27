771535dc7c676ba8ca7b125db5537231a813f928
adrian garcia <adriang1004@gmail.com>
2024-02-27 15:49:57 -0600

    created queries and mutations front-end

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
-
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import Navbar from './components/Navbar';
 
 function App() {
