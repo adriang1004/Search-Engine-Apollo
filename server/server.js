a3a74a482108fbb83533dc680530827b709d993e
adrian garcia <adriang1004@gmail.com>
2024-02-27 23:28:22 -0600

    test

--- server/server.js
+++ server/server.js
@@ -1,36 +1,36 @@
-const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
-const { ApolloServer } = require('apollo-server');
-const { expressMiddleware } = require('apollo-server-express');
+const { ApolloServer } = require('apollo-server-express');
 
 const { typeDefs, resolvers } = require('./schemas'); 
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 const startApolloServer = async () => {
-  await server.start();
+  await server.start(); 
+  server.applyMiddleware({ app, path: '/graphql' });
+
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
 
-  app.use('/graphql', expressMiddleware(server));
   app.use(routes);
+
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
     app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, '../client/dist/index.html'));
     });
   }
+
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`); // Updated log message
     });
   });
 };
